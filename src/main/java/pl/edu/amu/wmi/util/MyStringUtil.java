67f5727b7d2c29b14ac0d8686c69f9be2d34bebe
dagi12 <dagi12@o2.pl>
2018-03-02 12:43:26 +0100

    eryk-web-server-common master initial commit

---
+++ src/main/java/pl/edu/amu/wmi/util/MyStringUtil.java
@@ -0,0 +1,54 @@
+package pl.edu.amu.wmi.util;
+
+import java.net.URI;
+
+/**
+ * Stworzone przez Eryk Mariankowski dnia 21.07.2017.
+ */
+public class MyStringUtil {
+
+    private MyStringUtil() {
+    }
+
+    public static String getLastCharacters(String s, int i) {
+        if (s == null || s.length() < i) {
+            return null;
+        } else if (s.length() == i) {
+            return s;
+        }
+        return s.substring(s.length() - i);
+    }
+
+    public static String removeWhitespace(String s) {
+        if (s != null) {
+            return s.replaceAll("\\s+", "");
+        }
+        return null;
+    }
+
+    public static String toUpperCase(String s) {
+        if (s != null) {
+            return s.toUpperCase();
+        }
+        return null;
+    }
+
+    public static String extension(String fileName) {
+        String extension = "";
+
+        int i = fileName.lastIndexOf('.');
+        if (i > 0) {
+            extension = fileName.substring(i + 1);
+        }
+        return extension;
+    }
+
+    public static String firstAndSecondName(String firstName, String secondName) {
+        return firstName + " " + secondName;
+    }
+
+    public static String hostFromDataSourceUrl(String url) {
+        return URI.create(url.substring(5).split(";")[0].split("\\\\")[0]).getHost();
+    }
+
+}
---
+++ src/main/java/pl/edu/amu/wmi/util/date/DateUtil.java
@@ -0,0 +1,150 @@
+package pl.edu.amu.wmi.util.date;
+
+import pl.edu.amu.wmi.util.date.DatePair;
+import pl.edu.amu.wmi.util.date.DateRange;
+
+import java.sql.Timestamp;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class DateUtil {
+
+
+    public static int currentIntTimestamp() {
+        return (int) (new Date().getTime() / 1000);
+    }
+
+    public static Date getYearAgo() {
+        final Calendar calendar = Calendar.getInstance();
+        calendar.setTime(new Date());
+        calendar.add(Calendar.YEAR, -1);
+        return calendar.getTime();
+    }
+
+    public static Date getMonthAgo() {
+        final Calendar calendar = Calendar.getInstance();
+        calendar.setTime(new Date());
+        calendar.add(Calendar.MONTH, -1);
+        return calendar.getTime();
+    }
+
+    public static Date getMinuteAgo() {
+        final Calendar calendar = Calendar.getInstance();
+        calendar.setTime(new Date());
+        calendar.add(Calendar.MINUTE, -1);
+        return calendar.getTime();
+    }
+
+    public static int dayDifference(Timestamp dateFrom, Timestamp dateTo) {
+        long diff = dateTo.getTime() - dateFrom.getTime();
+        return (int) TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+    }
+
+    public static int dayDifference(Date dateFrom, Date dateTo) {
+        long diff = dateTo.getTime() - dateFrom.getTime();
+        return (int) TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+    }
+
+    public static int hourDifference(Timestamp dateFrom, Timestamp dateTo) {
+        long diff = dateTo.getTime() - dateFrom.getTime();
+        return (int) TimeUnit.MILLISECONDS.toHours(diff);
+    }
+
+    private static void clearDay(Calendar calendar) {
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+    }
+
+    private static void clearWeek(Calendar calendar) {
+        calendar.set(Calendar.DAY_OF_WEEK, calendar.getFirstDayOfWeek());
+        clearDay(calendar);
+    }
+
+    public static Date addDays(int days) {
+        return addDays(new Date(), days);
+    }
+
+    public static Date addDays(Date date, int days) {
+        final Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.add(Calendar.DATE, days);
+        return calendar.getTime();
+    }
+
+    private static void clearMonth(Calendar calendar) {
+        calendar.set(Calendar.DAY_OF_MONTH, 0);
+    }
+
+    public static DatePair getDateRange(DateRange dateRange) {
+        final Calendar calendar = Calendar.getInstance();
+        Date fromDate = new Date();
+        Date toDate = null;
+        calendar.setTime(fromDate);
+        switch (dateRange) {
+            case CURRENT_DAY:
+                clearDay(calendar);
+                fromDate = calendar.getTime();
+                calendar.add(Calendar.DATE, 1);
+                toDate = calendar.getTime();
+                break;
+            case CURRENT_WEEK:
+                clearWeek(calendar);
+                fromDate = calendar.getTime();
+                calendar.add(Calendar.DAY_OF_YEAR, 7);
+                toDate = calendar.getTime();
+                break;
+            case CURRENT_MONTH:
+                clearMonth(calendar);
+                fromDate = calendar.getTime();
+                calendar.add(Calendar.MONTH, 1);
+                toDate = calendar.getTime();
+                break;
+            case NEXT_DAY:
+                clearDay(calendar);
+                calendar.add(Calendar.DATE, 1);
+                fromDate = calendar.getTime();
+                calendar.add(Calendar.DATE, 1);
+                toDate = calendar.getTime();
+                break;
+            case NEXT_WEEK:
+                clearWeek(calendar);
+                calendar.add(Calendar.WEEK_OF_MONTH, 1);
+                fromDate = calendar.getTime();
+                calendar.add(Calendar.WEEK_OF_MONTH, 1);
+                toDate = calendar.getTime();
+                break;
+            case NEXT_MONTH:
+                clearMonth(calendar);
+                clearDay(calendar);
+                calendar.add(Calendar.MONTH, 1);
+                fromDate = calendar.getTime();
+                calendar.add(Calendar.MONTH, 1);
+                toDate = calendar.getTime();
+                break;
+            default:
+                break;
+        }
+        return new DatePair(fromDate, toDate);
+    }
+
+    public static Timestamp currentTimestamp() {
+        return new Timestamp(new Date().getTime());
+    }
+
+    public static boolean differentDay(Date date) {
+        Calendar cal1 = Calendar.getInstance();
+        Calendar cal2 = Calendar.getInstance();
+        cal1.setTime(date);
+        cal2.setTime(new Date());
+        return !(cal1.get(Calendar.YEAR) == cal2.get(Calendar.YEAR) &&
+                cal1.get(Calendar.DAY_OF_YEAR) == cal2.get(Calendar.DAY_OF_YEAR));
+    }
+
+    public static Timestamp toTimestamp(Date date) {
+        return new Timestamp(date.getTime());
+    }
+
+}
