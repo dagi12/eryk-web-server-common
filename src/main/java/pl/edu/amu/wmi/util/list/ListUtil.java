77ec49129c2fe717799c5cdbeec2856821de05d9
Eryk Mariankowski <dagi12@o2.pl>
2018-05-23 13:04:30 +0200

    003949

--- src/main/java/pl/edu/amu/wmi/util/list/ListUtil.java
+++ src/main/java/pl/edu/amu/wmi/util/list/ListUtil.java
@@ -1,7 +1,8 @@
 package pl.edu.amu.wmi.util.list;
 
 import java.util.List;
-import java.util.function.ToIntFunction;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /**
  * Stworzone przez Eryk Mariankowski dnia 19.04.18.
@@ -12,8 +13,9 @@ public class ListUtil {
 
     }
 
-    public static <T> boolean equalByProperty(List<T> a, List<T> b, ToIntFunction<? super T> mapper) {
-        return a.stream().mapToInt(mapper).equals(b.stream().mapToInt(mapper));
+    public static <T, R> boolean equalByProperty(List<T> a, List<T> b, Function<T, R> mapper) {
+        return a.stream().map(mapper).collect(Collectors.toList())
+                .equals(b.stream().map(mapper).collect(Collectors.toList()));
     }
 
 }
