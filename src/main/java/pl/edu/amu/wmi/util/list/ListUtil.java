cb9ad826091c04ee3c6a0503108ac9b3a927bdce
Eryk Mariankowski <dagi12@o2.pl>
2018-06-23 15:46:53 +0200

    004040 redirect deploy logs to swing console

--- src/main/java/pl/edu/amu/wmi/util/list/ListUtil.java
+++ src/main/java/pl/edu/amu/wmi/util/list/ListUtil.java
@@ -1,5 +1,8 @@
 package pl.edu.amu.wmi.util.list;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -16,6 +19,14 @@ public class ListUtil {
     public static <T, R> boolean equalByProperty(List<T> a, List<T> b, Function<T, R> mapper) {
         return a.stream().map(mapper).collect(Collectors.toList())
                 .equals(b.stream().map(mapper).collect(Collectors.toList()));
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T> T[] addToArray(T[] arr, T t) {
+        List<T> arrayList = new ArrayList<>(Arrays.asList(arr));
+        arrayList.add(t);
+        T[] newArr = (T[]) Array.newInstance(t.getClass(), arr.length + 1);
+        return arrayList.toArray(arrayList.toArray(newArr));
     }
 
 }
