a583ea3dff9f73a8f4d4ac8e1e80f6932f23a5f4
Eryk Mariankowski <dagi12@o2.pl>
2018-08-10 09:39:30 +0200

    003524 dodanie pozycji zakresu zlecenia na podglądzie

--- src/main/java/pl/edu/amu/wmi/controller/CrudController.java
+++ src/main/java/pl/edu/amu/wmi/controller/CrudController.java
@@ -1,6 +1,5 @@
 package pl.edu.amu.wmi.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,31 +9,25 @@ import pl.edu.amu.wmi.service.CrudService;
 /**
  * Stworzone przez Eryk Mariankowski dnia 18.06.18.
  */
-public abstract class CrudController<T> {
+public interface CrudController<T> {
 
-    @Autowired
-    private final CrudService<T> crudService;
-
-    @Autowired
-    protected CrudController(CrudService<T> crudService) {
-        this.crudService = crudService;
-    }
+    CrudService<T> getCrudService();
 
     @RequestMapping(method = RequestMethod.POST)
-    public T create(@RequestBody T t) {
-        crudService.create(t);
+    default T create(@RequestBody T t) {
+        getCrudService().create(t);
         return t;
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
-    public T update(@PathVariable int id, @RequestBody T t) {
-        crudService.update(id, t);
+    default T update(@PathVariable int id, @RequestBody T t) {
+        getCrudService().update(id, t);
         return t;
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-    public void delete(@PathVariable int id) {
-        crudService.delete(id);
+    default void delete(@PathVariable int id) {
+        getCrudService().delete(id);
     }
 
 
--- src/main/java/pl/edu/amu/wmi/service/CrudService.java
+++ src/main/java/pl/edu/amu/wmi/service/CrudService.java
@@ -1,39 +1,31 @@
 package pl.edu.amu.wmi.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-import pl.edu.amu.wmi.controller.CrudController;
 import pl.edu.amu.wmi.db.CommonEntityManager;
-import pl.edu.amu.wmi.util.ObjectUtil;
 
 /**
  * Stworzone przez Eryk Mariankowski dnia 18.06.18.
  */
-@Service
-@Transactional
-public class CrudService<T> {
+public interface CrudService<T> {
 
-    private final CommonEntityManager commonEntityManager;
-    private final Class<T> tClass;
+    CommonEntityManager getCommonEntityManager();
 
-    @Autowired
-    public CrudService(CommonEntityManager commonEntityManager) {
-        this.commonEntityManager = commonEntityManager;
-        this.tClass = ObjectUtil.getGenericType(this, CrudController.class, 0);
-    }
+    Class<T> gettClass();
 
-    public T create(T t) {
-        commonEntityManager.save(t);
+    default T create(T t) {
+        setDefaultValues(t);
+        getCommonEntityManager().save(t);
         return t;
     }
 
-    public T update(int id, T t) {
-        commonEntityManager.update(t);
+    default void setDefaultValues(T t) {
+    }
+
+    default T update(int id, T t) {
+        getCommonEntityManager().update(t);
         return t;
     }
 
-    public void delete(int id) {
-        commonEntityManager.delete(tClass, id);
+    default void delete(int id) {
+        getCommonEntityManager().delete(gettClass(), id);
     }
 }
