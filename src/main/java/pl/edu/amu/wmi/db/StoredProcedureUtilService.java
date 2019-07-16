aa5226675d93abf8c1d9566e14f32bfc55c6e642
Eryk Mariankowski <dagi12@o2.pl>
2019-07-16 12:44:00 +0200

    004891

--- src/main/java/pl/edu/amu/wmi/db/StoredProcedureUtilService.java
+++ src/main/java/pl/edu/amu/wmi/db/StoredProcedureUtilService.java
@@ -48,18 +48,19 @@ public class StoredProcedureUtilService {
     }
 
     private <T> Query getQuery(Class<T> tClass, String procedureName, String parametersToFill) {
+        String format1 = String.format("SELECT * FROM %s(%s)", procedureName, parametersToFill);
         if (tClass != null) {
             if (dataBaseConfig.isOracle()) {
                 String format = String.format("SELECT * FROM table(%s(%s))", procedureName, parametersToFill);
                 return entityManager.createNativeQuery(format, tClass);
             }
-            return entityManager.createNativeQuery("SELECT * FROM ?(?)", tClass);
+            return entityManager.createNativeQuery(format1, tClass);
         }
         if (dataBaseConfig.isOracle()) {
             String format = String.format("SELECT * FROM table(%s(%s))", procedureName, parametersToFill);
             return entityManager.createNativeQuery(format);
         }
-        return entityManager.createNativeQuery("SELECT * FROM ?(?)");
+        return entityManager.createNativeQuery(format1);
     }
 
     public <T> Query commonProcedure(String procedureName, Class<T> tClass, Object... parameters) {
@@ -70,9 +71,21 @@ public class StoredProcedureUtilService {
         return internalCommonProcedure(procedureName, null, parameters);
     }
 
+    // do not change that MappingException: No Dialect mapping for JDBC type: 1111
     @SuppressWarnings("squid:S2077")
     public void voidProcedure(String procedureName, Object... parameters) {
-        Query query = commonProcedure(procedureName, null, parameters);
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < parameters.length; i++) {
+            builder.append("?");
+            if (i != parameters.length - 1) {
+                builder.append(",");
+            }
+        }
+        String sqlString = "SELECT 1 FROM " + procedureName + "(" + builder.toString() + ")";
+        Query query = entityManager.createNativeQuery(sqlString);
+        for (int i = 0; i < parameters.length; ++i) {
+            query.setParameter(i + 1, parameters[i]);
+        }
         query.getSingleResult();
     }
 
